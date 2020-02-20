da0c8da84557e9a9ec23cece347953cdbaff291a
Abby Hudson <abbyeleanorhudson@gmail.com>
2020-02-20 18:17:57 +0100

    cocktail

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,23 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :cocktails do
+    resources :doses, only: [ :new, :create ]
+  end
+    resources :doses, only: [ :destroy]
 end
+
+# A user can see the list of cocktails
+# GET "cocktails"
+# A user can see the details of a given cocktail, with the dose needed for each ingredient
+# GET "cocktails/42"
+# A user can create a new cocktail
+# GET "cocktails/new"
+# POST "cocktails"
+# 4 - Routing, Controller, Views for Doses
+# A user can add a new dose (ingredient/description pair) to an existing cocktail
+# Checkout simple_form docs about f.association to easily create a select dropdown for our list of ingredients.
+# GET "cocktails/42/doses/new"
+# POST "cocktails/42/doses"
+# A user can delete a dose that belongs to an existing cocktail. How can we make a delete link again?
+# DELETE "doses/25"
+# Do we need an Ingr
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,26 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+require 'open-uri'
+require 'JSON'
+
+url = "https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list"
+json = open(url).read
+objs = JSON.parse(json)
+
+objs["drinks"].each do |object|
+  ingredient = object["strIngredient1"]
+  Ingredient.create(name: ingredient)
+end
+
+# url = "https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=Cocktail"
+# json = open(url).read
+# objs = JSON.parse(json)
+
+# objs["drinks"].each do |object|
+#   cocktail = object["strDrink"]
+#   image = object["strDrinkThumb"]
+#   Cocktail.create(name: cocktail)
+# end
