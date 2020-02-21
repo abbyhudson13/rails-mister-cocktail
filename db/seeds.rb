fd01fa63e16451bc0bfd5d43d233d1f9f3491d58
Abby Hudson <abbyeleanorhudson@gmail.com>
2020-02-21 15:23:34 +0100

    modified seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,7 +11,7 @@ require 'open-uri'
 require 'JSON'
 
 url = "https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list"
-json = open(url).read
+json = URI.open(url).read
 objs = JSON.parse(json)
 
 objs["drinks"].each do |object|
