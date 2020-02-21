d481acf93aa09a75136bd0d6e867cdcf6b4c2b44
Abby Hudson <abbyeleanorhudson@gmail.com>
2020-02-21 13:48:01 +0100

    remove delete function

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -13,11 +13,6 @@ class CocktailsController < ApplicationController
     @cocktail = Cocktail.new
   end
 
-  def destroy
-    @cocktail = Cocktail.find(params[:id])
-    @cocktail.destroy
-  end
-
   def create
     @cocktail = Cocktail.new(cocktail_params)
     if @cocktail.save
