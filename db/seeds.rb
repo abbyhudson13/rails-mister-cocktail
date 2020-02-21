af52517d7a29167311485b48f6c7cc93e7a86bc6
Abby Hudson <abbyeleanorhudson@gmail.com>
2020-02-21 15:43:48 +0100

    added dependant on ingredient

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,13 +6,23 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+puts "Deleting all ingredients"
+Ingredient.delete_all
 
 
+puts "fetching from api"
+url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+response = HTTParty.get(url)
+objs = response.parsed_response
 
+puts "Creating ingredients"
 objs["drinks"].each do |object|
   ingredient = object["strIngredient1"]
   Ingredient.create(name: ingredient)
 end
+
+puts "Done"
+puts "You now have #{Ingredient.count} ingredients"
 
 # url = "https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=Cocktail"
 # json = open(url).read
