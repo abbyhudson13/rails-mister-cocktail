26096b475acb1d9bd13b662a9f49cfcdbc8a7c6c
Abby Hudson <abbyeleanorhudson@gmail.com>
2020-02-22 17:05:51 +0100

    validates file

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -3,4 +3,8 @@ class Cocktail < ApplicationRecord
   has_many :doses, dependent: :destroy
   has_many :ingredients, through: :doses
   validates :name, presence: true, uniqueness: true
+  validates :photo, allow_blank: false, format: {
+  with: %r{\.gif|jpg|png}i,
+  message: 'must be a url for gif, jpg, or png image.'
+}
 end
