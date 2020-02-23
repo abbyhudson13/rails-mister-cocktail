c93728a1fbae51bd2a75f87e6b22223540f11b34
Abby Hudson <abbyeleanorhudson@gmail.com>
2020-02-23 15:58:53 +0100

    added active storage gem

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -3,5 +3,5 @@ class Cocktail < ApplicationRecord
   has_many :doses, dependent: :destroy
   has_many :ingredients, through: :doses
   validates :name, presence: true, uniqueness: true
-  validates :photo, attached: true
+  validates :photo, attached: true, content_type: ['image/png', 'image/jpg', 'image/jpeg'], size: { less_than: 100.megabytes , message: 'is too large' }
 end
